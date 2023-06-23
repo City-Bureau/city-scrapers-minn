8f6c40f538a66e507b30d5bc3a58f9ba9604f936
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:13:13 -0400

    fix timezone

--- city_scrapers/spiders/minn_audit_co.py
+++ city_scrapers/spiders/minn_audit_co.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnAuditCoSpider(CityScrapersSpider):
     name = "minn_audit_co"
     agency = "Audit Committee"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     today = datetime.today()
     fromDate = today.strftime("%B %d, %Y")
