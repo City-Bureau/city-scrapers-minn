8ba53a2fb6c94568da6f3d520222e6acdd853564
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:17:05 -0400

    fix timezone

--- city_scrapers/spiders/minn_char_co_rsw.py
+++ city_scrapers/spiders/minn_char_co_rsw.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnCharCoRswSpider(CityScrapersSpider):
     name = "minn_char_co_rsw"
     agency = "Charter Commission Rent Stabilization Work Group"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
