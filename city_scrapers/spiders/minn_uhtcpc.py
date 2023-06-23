fc66067450ac41a9f7a09178785c04f0d6d5c50a
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:27:56 -0400

    fix timezone

--- city_scrapers/spiders/minn_uhtcpc.py
+++ city_scrapers/spiders/minn_uhtcpc.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnUhtcpcSpider(CityScrapersSpider):
     name = "minn_uhtcpc"
     agency = "Upper Harbor Terminal Collaborative Planning Committee"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
