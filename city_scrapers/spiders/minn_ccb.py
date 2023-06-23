7ec2d22d28a36b8d86e96164ae62c30ccbf00588
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:15:27 -0400

    fix timezone

--- city_scrapers/spiders/minn_ccb.py
+++ city_scrapers/spiders/minn_ccb.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnCcbSpider(CityScrapersSpider):
     name = "minn_ccb"
     agency = "City Canvassing Board"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
