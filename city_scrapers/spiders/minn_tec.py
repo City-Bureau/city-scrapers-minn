cb4667e83750816e8f5e537d348cc795b09ff937
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:27:30 -0400

    fix timezone

--- city_scrapers/spiders/minn_tec.py
+++ city_scrapers/spiders/minn_tec.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnTecSpider(CityScrapersSpider):
     name = "minn_tec"
     agency = "Transgender Equity Council"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
