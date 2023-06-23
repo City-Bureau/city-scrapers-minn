bcf9e0912a0af571ebc9d79cee06b58036518111
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:21:30 -0400

    fix timezone

--- city_scrapers/spiders/minn_llssdab.py
+++ city_scrapers/spiders/minn_llssdab.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnLlssdabSpider(CityScrapersSpider):
     name = "minn_llssdab"
     agency = "Lyndale-Lake Special Service District Advisory Board"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
