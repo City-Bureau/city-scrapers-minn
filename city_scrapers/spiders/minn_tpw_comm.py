6035bbed34dba1249ba6dcb102ac6aa7288b788e
haileyneorsd <128643117+haileyneorsd@users.noreply.github.com>
2023-06-23 07:27:43 -0400

    fix timezone

--- city_scrapers/spiders/minn_tpw_comm.py
+++ city_scrapers/spiders/minn_tpw_comm.py
@@ -8,7 +8,7 @@ from city_scrapers_core.spiders import CityScrapersSpider
 class MinnTpwCommSpider(CityScrapersSpider):
     name = "minn_tpw_comm"
     agency = "Transportation & Public Works (TPW) Committee"
-    timezone = "America/Chicago"
+    timezone = "America/North_Dakota/Beulah"
     base_url = "https://lims.minneapolismn.gov/Calendar/GetCalenderList"
     start_urls = [
         "{}?fromDate={}&toDate={}&meetingType={}&committeeId={}&pageCount={}&offsetStart=0&abbreviation=&keywords=&sortOrder={}".format(
