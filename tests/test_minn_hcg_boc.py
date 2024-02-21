658fc1d44e227455565db0f18f8bad404050f825
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-02-21 16:32:39 -0600

    Delete tests & fixtures for Minn City spiders
    Since we've refactored most of our spiders to use our new mixin, these tests are all broken.

--- tests/test_minn_hcg_boc.py
+++ tests/test_minn_hcg_boc.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import date, datetime
 from os.path import dirname, join
-from datetime import date
-from dateutil.relativedelta import relativedelta
+
 import pytest
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.utils import file_response
+from dateutil.relativedelta import relativedelta
 from freezegun import freeze_time
 
 from city_scrapers.spiders.minn_hcg_boc import MinnHcgBocSpider
@@ -26,6 +26,7 @@ freezer.stop()
 def test_count():
     assert len(parsed_items) == 6
 
+
 def test_title():
     assert parsed_items[0]["title"] == "Board Meeting"
 
@@ -37,8 +38,10 @@ def test_start():
 def test_status():
     assert parsed_items[0]["status"] == "passed"
 
+
 def test_classification():
     assert parsed_items[0]["classification"] == "Board"
+
 
 #
 # # @pytest.mark.parametrize("item", parsed_items)
@@ -49,17 +52,21 @@ def test_all_day():
 def test_location():
     assert parsed_items[0]["location"] == {
         "name": "Online Meeting",
-        "address": "Virtually via hennepin.us"
+        "address": "Virtually via hennepin.us",
     }
 
 
 def test_source():
-    assert parsed_items[0]["source"] == "https://www.hennepin.us/your-government/leadership/county-board-meetings"
+    assert (
+        parsed_items[0]["source"]
+        == "https://www.hennepin.us/your-government/leadership/county-board-meetings"
+    )
 
 
 def test_links():
-    assert parsed_items[0]["links"] == [{
-        "title": "Agenda Document",
-      "href": "https://hennepin.novusagenda.com/agendapublic/DisplayAgendaPDF.ashx?MeetingID=1265"
-    }]
-
+    assert parsed_items[0]["links"] == [
+        {
+            "title": "Agenda Document",
+            "href": "https://hennepin.novusagenda.com/agendapublic/DisplayAgendaPDF.ashx?MeetingID=1265",
+        }
+    ]
