1d914d1d9b5b7439b1f33cbcc672a88798da2975
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:22 -0500

    Deprecate spider minn_esssdab

--- city_scrapers/spiders/minn_esssdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnEsssdabSpider(MinnCityMixin):
-    name = "minn_esssdab"
-    agency = "Eat Street Special Service District Advisory Board"
-    committee_id = 162
-    meeting_type = 2
