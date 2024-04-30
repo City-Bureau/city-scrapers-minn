25de2187328a2bbe40164e73afc0f76c6efbb5f1
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:36 -0500

    Deprecate spider minn_cassdab

--- city_scrapers/spiders/minn_cassdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnCassdabSpider(MinnCityMixin):
-    name = "minn_cassdab"
-    agency = "Central Avenue Special Service District Advisory Board"
-    committee_id = 152
-    meeting_type = 2
