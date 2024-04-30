b3aa40432013ad48d93c1628cf3ff44c1c15c51a
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:29 -0500

    Deprecate spider minn_svssdab

--- city_scrapers/spiders/minn_svssdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnSvssdabSpider(MinnCityMixin):
-    name = "minn_svssdab"
-    agency = "Stadium Village Special Service District Advisory Board"
-    committee_id = 163
-    meeting_type = 2
