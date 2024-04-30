5fdcf3e770419537d3f365711dcfd1a99843c9d1
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:43 -0500

    Deprecate spider minn_ex_co

--- city_scrapers/spiders/minn_ex_co.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnExCoSpider(MinnCityMixin):
-    name = "minn_ex_co"
-    agency = "Executive Committee"
-    committee_id = 7
-    meeting_type = 4
