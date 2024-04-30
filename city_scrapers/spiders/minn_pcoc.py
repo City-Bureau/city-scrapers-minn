c9b27e79c24574b5a4093004154e8d5727e7dc27
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:15:04 -0500

    Deprecate spider minn_pcoc

--- city_scrapers/spiders/minn_pcoc.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnPcocSpider(MinnCityMixin):
-    name = "minn_pcoc"
-    agency = "Police Conduct Oversight Commission"
-    committee_id = 215
-    meeting_type = 4
