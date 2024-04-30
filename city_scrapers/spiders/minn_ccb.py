994de13bb04ff293a911f14fc009bdea39176181
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:46 -0500

    Deprecate spider minn_ccb

--- city_scrapers/spiders/minn_ccb.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnCcbSpider(MinnCityMixin):
-    name = "minn_ccb"
-    agency = "City Canvassing Board"
-    committee_id = 165
-    meeting_type = 4
