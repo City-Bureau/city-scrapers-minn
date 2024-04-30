6f8994410bc941db56177be8a0e3e2a83413807f
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:32 -0500

    Deprecate spider minn_lhssdab

--- city_scrapers/spiders/minn_lhssdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnLhssdabSpider(MinnCityMixin):
-    name = "minn_lhssdab"
-    agency = "Linden Hills Special Service District Advisory Board"
-    committee_id = 159
-    meeting_type = 2
