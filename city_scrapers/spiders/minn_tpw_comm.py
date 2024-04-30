44c3439b53d766365802078b1726888f3cc4ee80
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:26 -0500

    Deprecate spider minn_tpw_comm

--- city_scrapers/spiders/minn_tpw_comm.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnTpwCommSpider(MinnCityMixin):
-    name = "minn_tpw_comm"
-    agency = "Transportation & Public Works (TPW) Committee"
-    committee_id = 13
-    meeting_type = 1
