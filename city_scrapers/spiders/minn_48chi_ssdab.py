79f6ae205101d2d45864e3eb0dcff530a6fa90bb
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:57 -0500

    Deprecate spider minn_48chi_ssdab

--- city_scrapers/spiders/minn_48chi_ssdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class Minn48chiSsdabSpider(MinnCityMixin):
-    name = "minn_48chi_ssdab"
-    agency = "48th & Chicago Special Service District Advisory Board"
-    committee_id = 153
-    meeting_type = 2
