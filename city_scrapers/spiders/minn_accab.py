e8cd965f4e095fc648c663aee1e3668ccd3042ef
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:12 -0500

    Deprecate spider minn_accab

--- city_scrapers/spiders/minn_accab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnAccabSpider(MinnCityMixin):
-    name = "minn_accab"
-    agency = "Animal Care and Control Advisory Board"
-    committee_id = 35
-    meeting_type = 2
