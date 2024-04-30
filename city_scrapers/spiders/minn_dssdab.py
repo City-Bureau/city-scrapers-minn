f0df79becd208e77c04fd55df577ab77b8715cf6
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-04-30 13:14:39 -0500

    Deprecate spider minn_dssdab

--- city_scrapers/spiders/minn_dssdab.py
+++
@@ -1,8 +0,0 @@
-from city_scrapers.mixins.minn_city import MinnCityMixin
-
-
-class MinnDssdabSpider(MinnCityMixin):
-    name = "minn_dssdab"
-    agency = "Dinkytown Special Service District Advisory Board"
-    committee_id = 155
-    meeting_type = 2
